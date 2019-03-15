caf7edd1dc9476d36576a4893e4b5efbe3435ef3
jinyuanqian <jinyuanqian@letto8.com>
2019-03-15 14:24:40 +0800

    add 代码

--- ups-pay-service/src/main/java/com/pgy/ups/pay/service/dao/UpsUserSignDubboDao.java
+++ ups-pay-service/src/main/java/com/pgy/ups/pay/service/dao/UpsUserSignDubboDao.java
@@ -4,6 +4,7 @@ import com.pgy.ups.pay.interfaces.entity.UpsAuthSignEntity;
 import com.pgy.ups.pay.interfaces.entity.UpsUserSignLogEntity;
 import com.pgy.ups.pay.interfaces.form.UpsUserSignForm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.List;
 @Repository
 public interface UpsUserSignDubboDao extends JpaRepository<UpsAuthSignEntity, Long> {
 
+
+    @Query(value = " select  * FROM ups_t_user_sign c  WHERE c.from_system = :#{#form.fromSystem} AND IF (:#{#form.orderType} !='',c.order_type = :#{#form.orderType}  ,1=1) AND IF (:#{#form.bankMd5} !='',c.bank_md5 = :#{#form.bankMd5} ,1=1)",nativeQuery = true)
     List<UpsAuthSignEntity> queryByForm(UpsUserSignForm form);
 
 }
