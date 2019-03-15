3d5db5d1ca4b3174f4eb72ff068bc4c628214d4e
jinyuanqian <jinyuanqian@letto8.com>
2019-03-15 10:20:53 +0800

    add 代码

--- ups-pay-service/src/main/java/com/pgy/ups/pay/service/dao/UpsOrderDubboDao.java
+++ ups-pay-service/src/main/java/com/pgy/ups/pay/service/dao/UpsOrderDubboDao.java
@@ -15,7 +15,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UpsOrderDubboDao extends JpaRepository<UpsOrderEntity, Long> {
 
-    @Query(value = "  FROM UpsOrderEntity c  WHERE c.fromSystem = :#{#form.fromSystem} AND       c.orderType = (CASE WHEN :#{#form.orderType} is null THEN true ELSE :#{#form.orderType} END)  AND c.bankMd5 = (CASE WHEN :#{#form.bankMd5} is null THEN true ELSE :#{#form.bankMd5} END)  ORDER BY c.id DESC")
+    @Query(value = " select  * FROM ups_t_order c  WHERE c.from_system = :#{#form.fromSystem} AND IF (:#{#form.orderType} !='',c.order_type = :#{#form.orderType}  ,1=1) AND IF (:#{#form.bankMd5} !='',c.bank_md5 = :#{#form.bankMd5} ,1=1)",nativeQuery = true)
     Page<UpsOrderEntity> getPage(@Param("form") UpsOrderForm form, Pageable pageable);
 
 }
+/**
+ *  AND       c.orderType = (CASE WHEN :#{#form.orderType} is null THEN true ELSE :#{#form.orderType} END)  AND c.bankMd5 = (CASE WHEN :#{#form.bankMd5} is null THEN true ELSE :#{#form.bankMd5} END)  ORDER BY c.id DESC
+ *
+ * **/
