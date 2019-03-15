1c7b7cc9838732ba506199ac479e07ce9396acc4
xuxu <xuxucainiao001@163.com>
2019-03-15 11:57:53 +0800

    修改

--- ups-pay-interface/src/main/java/com/pgy/ups/pay/interfaces/service/order/dubbo/UpsOrderService.java
+++ ups-pay-interface/src/main/java/com/pgy/ups/pay/interfaces/service/order/dubbo/UpsOrderService.java
@@ -1,10 +1,8 @@
 package com.pgy.ups.pay.interfaces.service.order.dubbo;
 
 import com.pgy.ups.common.page.PageInfo;
-import com.pgy.ups.pay.interfaces.entity.OrderPushEntity;
 import com.pgy.ups.pay.interfaces.entity.UpsOrderEntity;
 import com.pgy.ups.pay.interfaces.form.UpsOrderForm;
-import com.pgy.ups.pay.interfaces.model.UpsParamModel;
 
 /**
  * Ups订单业务逻辑
