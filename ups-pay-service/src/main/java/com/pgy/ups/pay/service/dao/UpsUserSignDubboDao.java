5dfaac06e34ef2692ead1c9d2c78449a9df94242
xuxu <xuxucainiao001@163.com>
2019-03-19 19:18:29 +0800

    功能完成

--- ups-pay-service/src/main/java/com/pgy/ups/pay/service/dao/UpsUserSignDubboDao.java
+++ ups-pay-service/src/main/java/com/pgy/ups/pay/service/dao/UpsUserSignDubboDao.java
@@ -13,8 +13,7 @@ import com.pgy.ups.pay.interfaces.form.UpsUserSignForm;
 @Repository
 public interface UpsUserSignDubboDao extends JpaRepository<UpsAuthSignEntity, Long> {
 
-
-    @Query(value = " select  * FROM ups_t_user_sign c  WHERE c.from_system = :#{#form.fromSystem} AND IF (:#{#form.signType} !='',c.sign_type = :#{#form.signType}  ,1=1) AND IF (:#{#form.bankMd5} !='',c.bank_md5 = :#{#form.bankMd5} ,1=1) AND IF (:#{#form.realNameMd5} !='',c.real_name_md5 = :#{#form.realNameMd5} ,1=1) AND IF (:#{#form.phoneNoMd5} !='',c.phone_no_md5 = :#{#form.phoneNoMd5} ,1=1) AND IF (:#{#form.identityMd5} !='',c.identity_md5 = :#{#form.identityMd5} ,1=1) AND IF (:#{#form.businessFlowNum} !='',c.business_flow_num = :#{#form.businessFlowNum} ,1=1)  ORDER BY c.create_time desc",nativeQuery = true)
-    Page<UpsAuthSignEntity> queryByForm(@Param("form") UpsUserSignForm form,Pageable pageable);
+	@Query(value = " select  * FROM ups_t_user_sign c  WHERE c.from_system = :#{#form.fromSystem} AND IF (:#{#form.signType} !='',c.sign_type = :#{#form.signType}  ,1=1) AND IF (:#{#form.bankMd5} !='',c.bank_md5 = :#{#form.bankMd5} ,1=1) AND IF (:#{#form.realNameMd5} !='',c.real_name_md5 = :#{#form.realNameMd5} ,1=1) AND IF (:#{#form.phoneNoMd5} !='',c.phone_no_md5 = :#{#form.phoneNoMd5} ,1=1) AND IF (:#{#form.identityMd5} !='',c.identity_md5 = :#{#form.identityMd5} ,1=1) AND IF (:#{#form.businessFlowNum} !='',c.business_flow_num = :#{#form.businessFlowNum} ,1=1)  AND IF (:#{#form.PayChannel} !='',c.pay_channel = :#{#form.PayChannel} ,1=1) AND IF (:#{#form.tradeNo} !='',c.trade_no = :#{#form.tradeNo} ,1=1) AND IF (:#{#form.status} !='',c.status = :#{#form.status} ,1=1) ORDER BY c.create_time desc", nativeQuery = true)
+	Page<UpsAuthSignEntity> queryByForm(@Param("form") UpsUserSignForm form, Pageable pageable);
 
 }
